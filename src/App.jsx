662240e6a12dfd6d1cae862eb6ac743ac5daa608
butch888 <git@stackblitz.com>
2025-03-04 22:38:17 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Count from './components/Count';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Count />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/store/counterReducer.js
@@ -0,0 +1,18 @@
+const initialState = {
+  value: 0,
+};
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { ...state, value: state.value + 1 };
+    case "DECREMENT":
+      return { ...state, value: state.value - 1 };
+    case "INCREMENT_BY_AMOUNT":
+      return { ...state, value: state.value + action.payload };
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
